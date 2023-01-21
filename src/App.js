d123694794530f3d0536890954c3c6cc2caa4db4
netiRussell <forworkneti@gmail.com>
2023-01-20 21:43:35 -0800

    Cart opening functionality implemented by useReduce and Context git push -u origin main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
+import CartContext from "./store/cart-context";
 
 import Header from "./components/Header/Header";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ItemsList from "./components/ItemsList/ItemsList";
 import Footer from "./components/Footer/Footer";
-
 import Backdrop from "./components/CartModal/CartModal";
 
+const data = [{ id: 1, status: "default", name: "Example", description: "This is an example of a dish that can be ordered here", price: 10, amount: 1 }];
+
+const cartReducer = function (prevValue, dispatchedValue) {
+  switch (dispatchedValue.type) {
+    case "CART_TOGGLE":
+      return { items: prevValue.items, isShown: !prevValue.isShown };
+    default:
+      return { items: prevValue.items, isShown: prevValue.isShown };
+  }
+};
+
 function App() {
+  const [cartState, cartStateDispatch] = useReducer(cartReducer, { items: data, isShown: false });
+
   return (
     <React.Fragment>
-      <Header />
-      <AboutUs />
-      <ItemsList />
-      <Footer />
-      {ReactDOM.createPortal(<Backdrop />, document.querySelector("body"))}
+      <CartContext.Provider value={{ data: cartState.items, isShown: cartState.isShown, cartStateDispatch: cartStateDispatch }}>
+        <Header />
+        <AboutUs />
+        <ItemsList />
+        <Footer />
+        {cartState.isShown && ReactDOM.createPortal(<Backdrop />, document.querySelector("body"))}
+      </CartContext.Provider>
     </React.Fragment>
   );
 }
--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -1,8 +1,17 @@
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 import styles from "./Cart.module.css";
 
 function Cart() {
+  const ctx = useContext(CartContext);
+
   return (
-    <button className={`button ${styles.button}`}>
+    <button
+      className={`button ${styles.button}`}
+      onClick={() => {
+        ctx.cartStateDispatch({ type: "CART_TOGGLE" });
+      }}
+    >
       <i className="fas fa-store"></i> Your cart
     </button>
   );
